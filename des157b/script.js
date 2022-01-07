ec7416471ca59e069520ff7ba49bdf82c50eb3f2
Jenny Wong <jywwong@ucdavis.edu>
2022-01-06 22:55:12 -0800

    home page created

---
+++ des157b/script.js
@@ -0,0 +1,34 @@
+(function () {
+  "use strict";
+
+  const button = document.querySelector("button");
+  const body = document.querySelector("body");
+  const banner = document.querySelector("#banner");
+  const bannerimg = document.querySelector("img");
+  const sections = document.querySelectorAll("section");
+  let mode = "dark";
+
+  button.addEventListener("click", function () {
+    if (mode === "dark") {
+      body.className = "switch";
+      banner.className = "switch";
+      button.className = "switch";
+      for (const section of sections) {
+        section.className = "switch";
+      }
+      mode = "light";
+
+      bannerimg.src = "images/dark-banner.jpg";
+    } else {
+      body.removeAttribute("class");
+      banner.removeAttribute("class");
+      button.removeAttribute("class");
+      for (const section of sections) {
+        section.removeAttribute("class");
+      }
+      mode = "dark";
+
+      bannerimg.src = "images/light-banner.jpg";
+    }
+  });
+})();
