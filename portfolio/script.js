f7d22ba576ff904bf1dd1b997c6503f6d664a51a
jenny270 <jywwong@ucdavis.edu>
2022-09-10 22:38:20 -0700

    updated

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,7 @@
 (function () {
   "use strict";
 
+  AOS.init();
   // ---------------------------- HEADER NAVIGATION ----------------------------
 
   const navBtn = document.querySelector("header i");
@@ -256,6 +257,10 @@
       allButtons[1].textContent = "development";
       allButtons[2].textContent = "UX / UI";
       for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener("mouseover", function () {
+          allButtons[i].backgroundColor = "#000";
+          allButtons[i].color = "#fff";
+        });
         allButtons[i].addEventListener("click", function () {
           allButtons[i].style.backgroundColor = "#000";
           allButtons[i].style.color = "#fff";
@@ -295,6 +300,52 @@
       // });
     }
   }
+
+  // CREDITS : https://www.cssscript.com/image-zoom-pan-hover-detail-view/
+  var addZoom = (target) => {
+    // (A) GET CONTAINER + IMAGE SOURCE
+    let container = document.getElementById(target),
+      imgsrc =
+        container.currentStyle || window.getComputedStyle(container, false);
+    imgsrc = imgsrc.backgroundImage.slice(4, -1).replace(/"/g, "");
+
+    // (B) LOAD IMAGE + ATTACH ZOOM
+    let img = new Image();
+    img.src = imgsrc;
+    img.onload = () => {
+      // (B1) CALCULATE ZOOM RATIO
+      let ratio = img.naturalHeight / img.naturalWidth,
+        percentage = ratio * 100 + "%";
+
+      // (B2) ATTACH ZOOM ON MOUSE MOVE
+      container.onmousemove = (e) => {
+        let rect = e.target.getBoundingClientRect(),
+          xPos = e.clientX - rect.left,
+          yPos = e.clientY - rect.top,
+          xPercent = xPos / (container.clientWidth / 100) + "%",
+          yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
+
+        Object.assign(container.style, {
+          backgroundPosition: xPercent + " " + yPercent,
+          backgroundSize: img.naturalWidth + "px",
+        });
+      };
+
+      // (B3) RESET ZOOM ON MOUSE LEAVE
+      container.onmouseleave = (e) => {
+        Object.assign(container.style, {
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        });
+      };
+    };
+  };
+
+  // (C) ATTACH FOLLOW ZOOM
+  window.onload = () => {
+    addZoom("user-flow-zoom");
+    addZoom("wire-flow-zoom");
+  };
 
   // allProjectsMobileNav(mobileView);
   mobileView.addEventListener("change", function () {
